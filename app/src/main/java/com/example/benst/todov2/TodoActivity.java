ee47bfc78c1a92a1e6a0a6e359c03fff3e1e5ed3
SW4RM <benstringfellow@live.co.uk>
2018-11-16 16:22:34 +0000

    Detail button added

--- app/src/main/java/com/example/benst/todov2/TodoActivity.java
+++ app/src/main/java/com/example/benst/todov2/TodoActivity.java
@@ -2,6 +2,7 @@ package com.example.benst.todov2;
 
 import android.content.Intent;
 import android.content.res.Resources;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,12 +10,20 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class TodoActivity extends AppCompatActivity {
     public static final String TAG = "TodoActivity";
     private String[] mTodos;
     private int mTodoIndex = 0;
-
+    private static final String IS_TODO_COMPLETE = "com.example.isTodoComplete";
+    private static final String TODO_INDEX = "com.example.TodoIndex";
+    private static final int IS_SUCCESS = 0;
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt(TODO_INDEX, mTodoIndex);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -45,9 +54,65 @@ public class TodoActivity extends AppCompatActivity {
         buttonNext.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                mTodoIndex += 1;
-                TodoTextView.setText(mTodos[mTodoIndex]);
+                mTodoIndex = (mTodoIndex + 1) % mTodos.length;
+                TodoTextView.setText(mTodos[mTodoIndex]); 
+                setTextViewComplete("");
+            }
+        });
+        Button buttonTodoDetail = (Button) findViewById(R.id.buttonTodoDetail);
+        buttonTodoDetail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = TodoDetailActivity.newIntent(TodoActivity.this, mTodoIndex);
+                startActivityForResult(intent, IS_SUCCESS);
             }
         });
     }
+
+
+    protected void onAvtivityResut(int requestCode, int resultCode, Intent intent) {
+        if (requestCode == IS_SUCCESS) {
+            if (intent != null) {
+                boolean isTodoComplete = intent.getBooleanExtra(IS_TODO_COMPLETE, false);
+                updateTodoComplete(isTodoComplete);
+            } else {
+                Toast.makeText(this, R.string.back_button_pressed, Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(this,R.string.request_code_mismatch,
+                    Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    private void updateTodoComplete (boolean is_todo_complete) {
+       final TextView textViewTodo;
+       textViewTodo = (TextView) findViewById(R.id.textViewTodo);
+
+       if (is_todo_complete) {
+           textViewTodo.setBackgroundColor(
+                   ContextCompat.getColor(this, R.color.backgroundSuccess));
+            textViewTodo.setTextColor(
+                    ContextCompat.getColor(this, R.color.colorSuccess));
+            setTextViewComplete("\u2713");
+       }
+
+    }
+    private void setTextViewComplete(String message){
+        final TextView textViewComplete;
+        textViewComplete = (TextView) findViewById(R.id.textViewComplete);
+        textViewComplete.setText(message);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+
+        if (requestCode == IS_SUCCESS){
+            boolean isTodoComplete = intent.getBooleanExtra(IS_TODO_COMPLETE, false);
+            updateTodoComplete(isTodoComplete);
+        }else{
+            Toast.makeText(this, R.string.back_button_pressed, Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
 }
